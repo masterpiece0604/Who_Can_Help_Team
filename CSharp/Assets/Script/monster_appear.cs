be2109147bc0850fd992add7433d54ae6951d24c
grace <lov011052@gmail.com>
2020-05-08 16:42:19 +0800

    怪物移動

--- CSharp/Assets/Script/monster_appear.cs
+++ CSharp/Assets/Script/monster_appear.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class monster_appear : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject monster;
+    GameObject clonemonster;
+    [Header("此怪物最高數量")]
+    public int Monster_most_total;
+    private int Monster_total;
+
+
     void Start()
     {
-        
+        InvokeRepeating("monstercreator", 5, 2);
+        // 重複執行(方案,第一次執行秒數,下次執行間隔秒數)
+        Monster_total = 0;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void monstercreator()
     {
-        
+        //clonemonster = Instantiate(monster) as GameObject;
+        int MonsterNum = 1;
+        // 一次生成一個
+        print("執行第" + Monster_total + "次");
+        if (Monster_total < 3)
+        {
+            for (int i = 0; i < MonsterNum; i++)
+            {
+                float x;
+                float z;
+
+                x = Random.Range(30, 35);
+                // 隨機生成X座標，範圍(30~35)
+
+                z = Random.Range(30, 35);
+                // 隨機生成Z座標，範圍(30~35)
+
+                Instantiate(monster, new Vector3(x, 0.6f, z), Quaternion.identity);
+                Monster_total++;
+                //clonemonster = Instantiate(monster) as GameObject;
+                if (Monster_total >= Monster_most_total)
+                {
+                    CancelInvoke();
+                }
+            }
+        }
+
+
+
     }
 }
--- CSharp/Assets/Script/monster_move.cs
+++ CSharp/Assets/Script/monster_move.cs
@@ -2,17 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class monster_move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("怪物偵測的半徑")]
+    public float alertRadius; //怪物偵測的半徑，如果角色走入這個半徑，怪物就要追擊
+    /// <summary>
+    /// 怪物的狀態
+    /// </summary>
+    // 原地不動,隨機走動,面向玩家,追擊玩家,返回原點
+    private enum MonsterState { STAND, WALK, WARN, CHASE, RETURN }
+
+    private MonsterState currentState = MonsterState.STAND;
+    // 預設怪物狀態為原地不走動
+
+    public int[] action_weight = { 3, 7 }; //設定原地不動跟隨機走動的機率
+
+    private float diatanceToPlay; //怪物跟玩家的距離
+    private float diatanceToInt; //怪物跟初始位置的距離
+    private float wanderRadius; // 怪物有沒有跑出範圍
+    private Quaternion targetRotation; //怪物的方向
+    private Vector3 initialPos; //怪物原始的位置
+    private float lastActTime; //上一次執行指令的時間
+    private float restTime = 5f; //兩個指令間的休息時間
+
+    private bool is_warn = false;
+    private bool is_run = false;
+
+    [Header("怪物的攻擊距離")]
+    public float attackRange;
+    [Header("怪物的移動速度")]
+    public float walkspeed;
+    private GameObject player;
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("怪獸");
+        initialPos = gameObject.GetComponent<Transform>().position;
+
+        RandomAct();
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void RandomAct()
     {
-        
+        //更新行動時間
+        lastActTime = Time.time;
+
+        // 設定一隨機數，讓怪物隨機移動
+        float number = Random.Range(0, 10);
+
+        if (number <= action_weight[0])
+        {
+            currentState = MonsterState.STAND;
+
+            print("怪物靜止中");
+
+            // 如果有動作動畫可以放在這裡
+        }
+        else if (number > action_weight[0])
+        {
+            currentState = MonsterState.WALK;
+            targetRotation = Quaternion.Euler(0, Random.Range(1, 5) * 90, 0);
+
+            print("怪物走動中");
+        }
     }
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case MonsterState.STAND:
+                if (Time.time - lastActTime > restTime)
+                {
+                    RandomAct();
+                }
+                break;
+
+            case MonsterState.WALK:
+                transform.Translate(Vector3.forward * Time.deltaTime * walkspeed);
+                transform.localRotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.1f);
+                print("轉向值:" + transform.rotation + "轉向值2:" + targetRotation);
+
+
+                if (Time.time - lastActTime > restTime)
+                {
+                    RandomAct();
+                }
+                break;
+        }
+    }
+
+
 }
