3bb7ba7f429526cb63b1cd0b4b0c7925f346a678
paul61302 <paul61302@gmail.com>
2020-05-15 08:38:38 +0800

    123

--- CSharp/Assets/Inventory/InventoryScripts/Inventory.cs
+++ CSharp/Assets/Inventory/InventoryScripts/Inventory.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/New Invertory")]
-=======
 [CreateAssetMenu(fileName = "New Invertory", menuName = "Inventory/New Invertory")]
->>>>>>> 道具欄開發
+
 public class Inventory : ScriptableObject
 {
     public List<Item> itemList = new List<Item>();
