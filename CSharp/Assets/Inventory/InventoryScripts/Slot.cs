b8409a2a4737058ff807e726c3890721209386f8
陳宛昀 <lov011052@gmail.com>
2020-05-31 19:57:56 +0800

    0531

--- CSharp/Assets/Inventory/InventoryScripts/Slot.cs
+++ CSharp/Assets/Inventory/InventoryScripts/Slot.cs
@@ -1,28 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
+    // 空格ID
+    public int slotID;
     public Item slotItem;
     public Image slotImage;
     public Text slotNum;
-    
+    public string slotInfo;
+
+    public bool controlText = false;
+    [Header("物品介紹")]
+    public Image Item_Information;
+    public Text ItemInfoText;
 
     public GameObject itemInSlot;
 
-    
-    /*private void OnMouseExit()
-    {
-        itemInformation.gameObject.SetActive(false);
-    }*/
 
     /*public void ItemDescription()
     {
         InventoryManager.UpdateItemInfo(slotItem.itemInfo);
     }*/
+    
+    void Update()
+    {
+        Item_Information.gameObject.SetActive(controlText);
+        //InventoryManager.UpdateItemInfo(slotItem.itemInfo);
+    }
 
+
+
+    public void clickItem()
+    {
+        
+        controlText = !controlText;
+        InventoryManager.UpdateItemInfo(slotInfo);
+        ItemInfoText.text = slotInfo;
+
+
+        //transform.SetParent(transform.parent.parent, true);
+
+        //ItemInfoText.text = (slotItem.itemInfo);
+        //UpdateItemInfo(slotItem.itemInfo);
+        //print(slotItem.itemInfo);
+    }
+    
 
     public void SetupSlot(Item item)
     {
@@ -34,6 +60,7 @@ public class Slot : MonoBehaviour
 
         slotImage.sprite = item.itemImage;
         slotNum.text = item.itemHold.ToString();
+        slotInfo = item.itemInfo;
     }
 
    
