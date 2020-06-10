02d1aabe1fa930e29b6bd13f9a200e72299fda62
paul61302 <paul61302@gmail.com>
2020-06-10 16:18:34 +0800

    123

--- CSharp/Assets/Script/Scene_Controll.cs
+++ CSharp/Assets/Script/Scene_Controll.cs
@@ -3,6 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Controll : MonoBehaviour
 {
+    public GameObject[] MenuButton;
+    public GameObject ChooseImage;
+    public bool buttons;
+    public bool ChooseImage1;
+
+    private void Start()
+    {
+        ChooseImage1 = true;
+    }
+
+    /// <summary>
+    /// 滑鼠移入
+    /// </summary>
+    public void P_State()
+    {
+        ChooseImage1 = !ChooseImage1;
+        buttons = !buttons;
+        ChooseImage.gameObject.SetActive(ChooseImage1);
+        MenuButton[0].gameObject.SetActive(buttons);
+    }
+
     public void New_Game()
     {
         SceneManager.LoadScene("動起來測試");
@@ -33,10 +54,17 @@ public class Scene_Controll : MonoBehaviour
         SceneManager.LoadScene("讀取進度");
     }
 
+    public void Difficalty()
+    {
+        SceneManager.LoadScene("困難度"); 
+    }
+
     /*public void HotKey()
     {
         SceneManager.LoadScene("按鍵設定");
     }*/
+
+    
 
     #region 遊戲關閉
     public void Quit()
