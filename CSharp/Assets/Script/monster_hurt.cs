42444a2d413c0400267b44d0e7f60c3d583de31d
paul61302 <paul61302@gmail.com>
2020-07-01 16:07:28 +0800

    123

--- CSharp/Assets/Script/monster_hurt.cs
+++ CSharp/Assets/Script/monster_hurt.cs
@@ -8,17 +8,10 @@ public class monster_hurt : MonoBehaviour
     public Image HP;
     public Ray ray;
     public Camera main_camera;
-
     /// <summary>
     /// 判斷是否正在攻擊
     /// </summary>
-    private bool Is_Atk;
-
-    [Header("生成怪物的空物件")]
-    public GameObject monster_empty;
-
-
-
+    
     private float Last_Attack;
 
     private void Start()
@@ -26,8 +19,7 @@ public class monster_hurt : MonoBehaviour
         Last_Attack = Time.time;
         Role = GameObject.FindGameObjectWithTag("Player");
         main_camera = Camera.FindObjectOfType<Camera>();
-        monster_empty = GameObject.FindGameObjectWithTag(gameObject.tag + "空物件");
-
+        
     }
     
 
@@ -36,13 +28,11 @@ public class monster_hurt : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.W) && Vector3.Distance(Role.transform.position, transform.position) < 1f)
         {
             MonsterHurt();
-            Is_Atk = false;
-        }
+                   }
         if (Input.GetMouseButtonDown(1) && (Vector3.Distance(Role.transform.position, transform.position) < 1f))
         {
             print("執行滑鼠攻擊");
             Mouse_atk();
-            Is_Atk = false;
         }
 
     }
@@ -51,15 +41,11 @@ public class monster_hurt : MonoBehaviour
         if ((Time.time - Last_Attack) > 1f)
         {
 
-            Is_Atk = true;
-
             Last_Attack = Time.time;
             monster.HP -= Role.GetComponent<Role_attak>().WAttak;
             HP.fillAmount = ((int)monster.HP - Role.GetComponent<Role_attak>().WAttak) / 100;
             if (HP.fillAmount <= 0)
             {
-
-                monster_empty.GetComponent<monster_appear>().monstercreator(2);
 
                 Destroy(gameObject);
 
@@ -84,9 +70,7 @@ public class monster_hurt : MonoBehaviour
                 if (HP.fillAmount <= 0)
                 {
 
-                    monster_empty.GetComponent<monster_appear>().monstercreator(2);
-
-                    Destroy(gameObject);
+                   Destroy(gameObject);
 
                 }
                 print("滑鼠攻擊");
