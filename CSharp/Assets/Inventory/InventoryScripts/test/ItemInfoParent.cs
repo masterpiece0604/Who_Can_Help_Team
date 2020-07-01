cba1f89727d9b489b33722afff23ac0a005d7dcc
paul61302 <paul61302@gmail.com>
2020-07-01 10:31:17 +0800

    123

---
+++ CSharp/Assets/Inventory/InventoryScripts/test/ItemInfoParent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemInfoParent : MonoBehaviour
+{
+    public Transform informationOldParent;
+    public Slot slot;
+
+    /*private void Update()
+    {
+
+        if (slot.controlText == true)
+        {
+            informationOldParent = transform.parent;
+            transform.SetParent(transform.parent.parent);
+        }
+        else
+        {
+            transform.SetParent(informationOldParent);
+            transform.position = informationOldParent.position;
+        }
+    }*/
+}
---
+++ CSharp/Assets/Script/cam_ref.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class cam_ref : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
