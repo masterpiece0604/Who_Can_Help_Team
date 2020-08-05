a8d93bca66364dac19f02b3e8fc7d6c1d8d65315
grace <lov011052@gmail.com>
2020-08-05 16:37:34 +0800

    0805

--- CSharp/Assets/Script/time_count.cs
+++ CSharp/Assets/Script/time_count.cs
@@ -9,20 +9,38 @@ public class time_count : MonoBehaviour
     public static int timer_date=1;
     public Text timer_count;
     private float DateTime;
+    private float LightTime;
     static time_count tc;
+
+    public Light Sunlight;
 
 
     void Start()
     {
        timer_count.text ="01" ;
-        //每8分鐘計算一天，先測試每分鐘計算一天
+        LightTime = Time.time;
         DateTime = Time.time;
         tc = this;
 
     }
-    private void Update()
+  
+    private void FixedUpdate()
     {
-        if(Time.time- DateTime > 480f)
+        if (Time.time - LightTime > 0.1f)
+        {
+            
+            if(Sunlight.transform.rotation.x>180)
+            {
+                Sunlight.transform.Rotate(Vector3.right/6f);
+            }
+            else
+            {
+                Sunlight.transform.Rotate(Vector3.right/30f);
+                LightTime = Time.time;
+            }
+        }
+
+        if(Time.time- DateTime > 600f)
         {
             Time_Date();
         }
@@ -33,7 +51,7 @@ public class time_count : MonoBehaviour
         timer_date++;
         tc.timer_count.text = timer_date / 10 + "" + timer_date % 10;
         tc.DateTime = Time.time;
-
-
     }
+
+
 }
