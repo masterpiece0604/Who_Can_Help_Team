1835d2e73e51a3bd399f010716631a4dcf21a441
grace <lov011052@gmail.com>
2020-08-11 14:55:50 +0800

    0811

---
+++ CSharp/Assets/Script/儲存資料/DataManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+
+    public static PlayData data;
+    public static bool load;
+    public Inventory bag;
+        
+    public void OldGame()
+    {
+        load = true;
+    }
+
+    public void SaveGame()
+    {
+        WriteData();
+    }
+
+
+    public void ReadData()
+    {
+       
+    }
+    public void WriteData()
+    {
+        Transform target = GameObject.FindGameObjectWithTag("Player").transform;
+        Role_quality player = GameObject.FindGameObjectWithTag("Player").GetComponent<Role_quality>();
+       
+
+        data.pos = target.position;
+        data.health = player.health;
+        data.hungry = player.hungry;
+        data.guilt = player.guilt;
+        data.sick = player.sick;
+        data.bag = bag;
+
+        string json = JsonUtility.ToJson(data);
+
+        PlayerPrefs.SetString("儲存資料1",json);
+
+    }
+
+}
---
+++ CSharp/Assets/Script/儲存資料/PlayData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public struct PlayData
+{
+    public Vector3 pos;
+    public Inventory bag;
+    public float health, hungry, guilt, sick;
+}
